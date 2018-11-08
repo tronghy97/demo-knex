4d9289e4f5434716e839335279af757e47a81537
trongtq <tronghy97@gmail.com>
2018-11-08 11:02:49 +0700

    sua file resolver và typeDef

--- src/schemas/RoleSchema/typeDef.ts
+++ src/schemas/RoleSchema/typeDef.ts
@@ -1,11 +1,4 @@
 export const typeDef = `
-    extend type Query {
-        
-    }
-
-    extend type Mutation {
-        createRole(name: String): Member
-    }
     type Role {
         id: ID,
         name: String
